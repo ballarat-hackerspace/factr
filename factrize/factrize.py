f93973beaebb9ffd5906ee89bb6794e722b7db8e
Ubuntu <josh@noisymime.org>
2016-07-31 03:14:26 +0000

    Working with backend

--- factrize/factrize.py
+++ factrize/factrize.py
@@ -1,20 +1,31 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response, make_response
+from json import dumps
 
 from generate_sentence import generate_sentence
 
 app = Flask(__name__)
 
 
-@app.route('/create_sentence', methods=["GET", "POST"])
+@app.route('/create_sentence', methods=["GET", "POST", "OPTIONS"])
 def just_get_it_working():
 
-    print(request.json)
+    if request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return resp
 
-    results = {"text": generate_sentence(request.json)}
 
-    return jsonify(results)
-
-
+    results = []
+    for object in request.json:
+        results.append({"text": generate_sentence(object)})
+    
+    #resp = jsonify(results)
+    resp = make_response(dumps(results))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- factrize/generate_sentence.py
+++ factrize/generate_sentence.py
@@ -11,38 +11,62 @@ reply_forms_notime.append("{value} {attribute}!? That's right, take a look at th
 reply_forms_notime.append("Nearby, you'll see a {category} with {value} {attribute}.")
 reply_forms_notime.append("Did you know that the nearest {category} has an {attribute} value of {value}.")
 reply_forms_notime.append("Did you know that the nearest {category} has an {value} {attribute}s?")
-
+reply_forms_notime.append("There's a {singular} just {distance} metres from you. {quip}")
 
 reply_forms_time = []
-reply_forms_time.append("Since {datetime_value}, the {category} near you has a {attribute} value of {value}.")
-reply_forms_time.append("Did you know that since {datetime_value}, the {category} near you has a {attribute} value of {value}.")
-reply_forms_time.append("On {datetime_value}, the {category} near you has a {attribute} value of {value}.")
-reply_forms_time.append("Did you know that on {datetime_value}, the {category} near you has a {attribute} value of {value}.")
+reply_forms_time.append("In the last {time_period} days, the {value_type} of {category} is {value}. {quip}")
+reply_forms_time.append("Did you know that since {time_period}, the {category} near you has a {attribute} value of {value}.")
+reply_forms_time.append("On {time_period}, the {category} near you has a {attribute} value of {value}.")
+reply_forms_time.append("Did you know that on {time_period}, the {category} near you has a {attribute} value of {value}.")
+reply_forms_time.append("There's a {singular} just {distance} metres from you. {quip}")
 
+reply_forms_avg = []
+reply_forms_avg.append("The average {category} in this area is $value. {quip}")
+reply_forms_avg.append("There's a {singular} just {distance} metres from you. {quip}")
 
+reply_forms_sum = []
+#reply_forms_sum.append("In total {category} in this area is $value. {quip}")
+reply_forms_sum.append("Over the last {time_period} days, nearby {plural} has totalled {value}. {quip}")
+reply_forms_sum.append("There's a {singular} just {distance} metres from you. {quip}")
 
 def generate_sentence(input_data):
 
     category = input_data['category']
-    datetime_value = input_data.get('datetime', None)
-    attribute = random.choice(input_data['attributes'])
+    plural = input_data['category']['plural']
+    singular = input_data['category']['singular']
+    # time_period = input_data.get('datetime', None)
+    time_period = input_data['time_period']
+    quip = input_data['quip']
+    attribute = input_data['value_name']
+    data_type = input_data['type']
+    try:
+        distance = round(float(input_data['distance']),0)
+    except TypeError:
+        distance = ""
+    
+    value = input_data['value']
 
     # IBM Watson (or similar) goes here, but the training couldn't be done to a reliable level in time.
     reply_options = None
     if awesome_sentence_generation_not_working:
+        if "avg" in data_type:
+            reply_options = reply_forms_avg
         # Choose either a temporal or non-temporal type
-        if "datetime" in input_data and input_data['datetime']:
+        elif "sum" in data_type:
+            reply_options = reply_forms_sum
+        elif "datetime" in input_data and input_data['datetime']:
             reply_options = reply_forms_time
             # Format datetime to be human readable
             dt = None
-            if isinstance(datetime_value, int):
-                dt = datetime.fromtimestamp(datetime_value)
-            elif not isinstance(datetime_value, datetime):
-                dt = dup.parse(datetime_value)
-            datetime_value = dt.strftime("%A %d %M, %Y")
+            if isinstance(time_period, int):
+                dt = datetime.fromtimestamp(time_period)
+            elif not isinstance(time_period, datetime):
+                dt = dup.parse(time_period)
+            time_period = dt.strftime("%A %d %M, %Y")
         else:
             reply_options = reply_forms_notime
 
     # Choose a reply form at random and populate it
-    return random.choice(reply_options).format(category=category, attribute=attribute['name'],
-                                               value=attribute['value'], datetime_value=datetime_value)
+    return random.choice(reply_options).format(category=category, attribute=attribute,
+                                               value=value, time_period=time_period, plural=plural,
+                                               distance=distance, singular=singular, quip=quip)
